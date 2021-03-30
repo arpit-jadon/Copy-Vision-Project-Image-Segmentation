17790f77debd54ec8de092bee92cdd64da21cca4
Arpit Jadon <arpitjadonamu@gmail.com>
2021-03-31 00:29:22 +0200

    Added Comments for DeepLabV3 Code

--- Final_Codes_Task_2_3/model.py
+++ Final_Codes_Task_2_3/model.py
@@ -10,18 +10,19 @@ import torch.nn.functional as F
 # **resnet**
 
 def make_layer(block, in_channels, channels, num_blocks, stride=1, dilation=1):
-    strides = [stride] + [1]*(num_blocks - 1) # (stride == 2, num_blocks == 4 --> strides == [2, 1, 1, 1])
+    strides = [stride] + [1]*(num_blocks - 1) 
 
     blocks = []
     for stride in strides:
         blocks.append(block(in_channels=in_channels, channels=channels, stride=stride, dilation=dilation))
         in_channels = block.expansion*channels
 
-    layer = nn.Sequential(*blocks) # (*blocks: call with unpacked list entires as arguments)
+    layer = nn.Sequential(*blocks) 
 
     return layer
 
 
+# Defining the block to be used in the resnet18 as layer 4 and 5
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -44,26 +45,28 @@ class BasicBlock(nn.Module):
             self.downsample = nn.Sequential()
 
     def forward(self, x):
-        # (x has shape: (batch_size, in_channels, h, w))
+        # (input shape: (batch_sz, in_channels, h, w))
 
-        out = F.relu(self.bn1(self.conv1(x))) # (shape: (batch_size, channels, h, w) if stride == 1, (batch_size, channels, h/2, w/2) if stride == 2)
-        out = self.bn2(self.conv2(out)) # (shape: (batch_size, channels, h, w) if stride == 1, (batch_size, channels, h/2, w/2) if stride == 2)
+        out = F.relu(self.bn1(self.conv1(x))) 
+        out = self.bn2(self.conv2(out)) 
 
-        out = out + self.downsample(x) # (shape: (batch_size, channels, h, w) if stride == 1, (batch_size, channels, h/2, w/2) if stride == 2)
+        out = out + self.downsample(x) 
 
-        out = F.relu(out) # (shape: (batch_size, channels, h, w) if stride == 1, (batch_size, channels, h/2, w/2) if stride == 2)
+        out = F.relu(out) 
 
         return out
 
 
+# Block for basical ResNet18 definition with pretrained weights loading
+# We pass an input of size (batch_sz, 3, h, w) while take the output from the 5th layer
 class ResNet_BasicBlock_OS8(nn.Module):
     def __init__(self):
         super(ResNet_BasicBlock_OS8, self).__init__()
 
         resnet = models.resnet18()
-        # load pretrained model:
+        # loading pretrained resnet18 model
         resnet.load_state_dict(torch.load("./pretrained_model/resnet18.pth"))
-        # remove fully connected layer, avg pool, layer4 and layer5:
+        # removing the fully connected layer, avg pool layer, layer4 and the  layer5
         self.resnet = nn.Sequential(*list(resnet.children())[:-4])
 
         num_blocks_layer_4 = 2
@@ -74,13 +77,13 @@ class ResNet_BasicBlock_OS8(nn.Module):
         self.layer5 = make_layer(BasicBlock, in_channels=256, channels=512, num_blocks=num_blocks_layer_5, stride=1, dilation=4)
 
     def forward(self, x):
-        # (x has shape (batch_size, 3, h, w))
+        # (input shape : (batch_sz, 3, h, w))
 
-        # pass x through (parts of) the pretrained ResNet:
-        c3 = self.resnet(x) # (shape: (batch_size, 128, h/8, w/8)) (it's called c3 since 8 == 2^3)
+        # passing input through the pretrained ResNet:
+        c3 = self.resnet(x) 
 
-        output = self.layer4(c3) # (shape: (batch_size, 256, h/8, w/8))
-        output = self.layer5(output) # (shape: (batch_size, 512, h/8, w/8))
+        output = self.layer4(c3) 
+        output = self.layer5(output) 
 
         return output
 
@@ -89,7 +92,7 @@ def ResNet18_OS8():
     return ResNet_BasicBlock_OS8()
 
 
-# **ASPP**
+# **ASPP [Atrous Spatial Pyramid Pooling]**
 
 class ASPP(nn.Module):
     def __init__(self, num_classes):
@@ -112,29 +115,32 @@ class ASPP(nn.Module):
         self.conv_1x1_2 = nn.Conv2d(512, 256, kernel_size=1)
         self.bn_conv_1x1_2 = nn.BatchNorm2d(256)
 
-        self.conv_1x1_3 = nn.Conv2d(1280, 256, kernel_size=1) # (1280 = 5*256)
+        self.conv_1x1_3 = nn.Conv2d(1280, 256, kernel_size=1) 
         self.bn_conv_1x1_3 = nn.BatchNorm2d(256)
 
         self.conv_1x1_4 = nn.Conv2d(256, num_classes, kernel_size=1)
 
     def forward(self, feature_map):
-        # (feature_map has shape (batch_size, 512, h/16, w/16)) (assuming self.resnet is ResNet18_OS16 or ResNet34_OS16. If self.resnet instead is ResNet18_OS8 or ResNet34_OS8, it will be (batch_size, 512, h/8, w/8))
 
-        feature_map_h = feature_map.size()[2] # (== h/16)
-        feature_map_w = feature_map.size()[3] # (== w/16)
-
-        out_1x1 = F.relu(self.bn_conv_1x1_1(self.conv_1x1_1(feature_map))) # (shape: (batch_size, 256, h/16, w/16))
-        out_3x3_1 = F.relu(self.bn_conv_3x3_1(self.conv_3x3_1(feature_map))) # (shape: (batch_size, 256, h/16, w/16))
-        out_3x3_2 = F.relu(self.bn_conv_3x3_2(self.conv_3x3_2(feature_map))) # (shape: (batch_size, 256, h/16, w/16))
-        out_3x3_3 = F.relu(self.bn_conv_3x3_3(self.conv_3x3_3(feature_map))) # (shape: (batch_size, 256, h/16, w/16))
-
-        out_img = self.avg_pool(feature_map) # (shape: (batch_size, 512, 1, 1))
-        out_img = F.relu(self.bn_conv_1x1_2(self.conv_1x1_2(out_img))) # (shape: (batch_size, 256, 1, 1))
-        out_img = F.upsample(out_img, size=(feature_map_h, feature_map_w), mode="bilinear") # (shape: (batch_size, 256, h/16, w/16))
-
-        out = torch.cat([out_1x1, out_3x3_1, out_3x3_2, out_3x3_3, out_img], 1) # (shape: (batch_size, 1280, h/16, w/16))
-        out = F.relu(self.bn_conv_1x1_3(self.conv_1x1_3(out))) # (shape: (batch_size, 256, h/16, w/16))
-        out = self.conv_1x1_4(out) # (shape: (batch_size, num_classes, h/16, w/16))
+        feature_map_h = feature_map.size()[2] 
+        feature_map_w = feature_map.size()[3] 
+        
+        # One 1x1 conv followed by three 3x3 conv with dilation rates of
+        # 6, 12, and 18 respectively.
+        out_1x1 = F.relu(self.bn_conv_1x1_1(self.conv_1x1_1(feature_map))) 
+        out_3x3_1 = F.relu(self.bn_conv_3x3_1(self.conv_3x3_1(feature_map))) 
+        out_3x3_2 = F.relu(self.bn_conv_3x3_2(self.conv_3x3_2(feature_map))) 
+        out_3x3_3 = F.relu(self.bn_conv_3x3_3(self.conv_3x3_3(feature_map))) 
+        
+        # Using average pooling to get the global context
+        out_img = self.avg_pool(feature_map) 
+        out_img = F.relu(self.bn_conv_1x1_2(self.conv_1x1_2(out_img))) 
+        out_img = F.upsample(out_img, size=(feature_map_h, feature_map_w), mode="bilinear") 
+        
+        # Concatenating all the features
+        out = torch.cat([out_1x1, out_3x3_1, out_3x3_2, out_3x3_3, out_img], 1) 
+        out = F.relu(self.bn_conv_1x1_3(self.conv_1x1_3(out))) 
+        out = self.conv_1x1_4(out) # Getting the final logits
 
         return out
 
@@ -151,16 +157,20 @@ class DeepLabV3(nn.Module):
         self.aspp = ASPP(num_classes=self.num_classes) 
 
     def forward(self, x):
-        # (x has shape (batch_size, 3, h, w))
+        # input x has the shape (batch_size, 3, h, w)
 
         h = x.size()[2]
         w = x.size()[3]
-
-        feature_map = self.resnet(x) # (shape: (batch_size, 512, h/16, w/16)) (assuming self.resnet is ResNet18_OS16 or ResNet34_OS16. If self.resnet is ResNet18_OS8 or ResNet34_OS8, it will be (batch_size, 512, h/8, w/8). If self.resnet is ResNet50-152, it will be (batch_size, 4*512, h/16, w/16))
-
-        output = self.aspp(feature_map) # (shape: (batch_size, num_classes, h/16, w/16))
-
-        output = F.upsample(output, size=(h, w), mode="bilinear") # (shape: (batch_size, num_classes, h, w))
+        
+        # Getting the feature map after passing the input through ResNet18
+        feature_map = self.resnet(x) 
+        
+        # Passing the feature map through ASPP
+        output = self.aspp(feature_map) 
+        
+        # Upsampling the final logits we get from ASPP to the original
+        # Image size using bilinear upsampling
+        output = F.upsample(output, size=(h, w), mode="bilinear") 
 
         return output
 
